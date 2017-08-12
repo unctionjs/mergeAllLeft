dad44b1c53a30db28e861eb9684ea477c2faa1b4
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:18 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import mapValuesWithValueKey from "./source"
+import mergeAllLeft from "./source"
 
 test(({same, end}) => {
   same(
