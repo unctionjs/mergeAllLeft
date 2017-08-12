a7f274290e89462beea1e27ceeb9bfcc34c47aa0
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:06:18 -0700

    Don't bother using the extension, eslint complains

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import mergeAllLeft from "./source.js"
+import mapValuesWithValueKey from "./source"
 
 test(({same, end}) => {
   same(
