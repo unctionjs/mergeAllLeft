88de7dda5accdea3a83b3e888d36cabf8da7cd55
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-13 21:14:33 -0700

    Using an internal package

--- source.js
+++ source.js
@@ -1,9 +1,10 @@
 import reduceValues from "@unction/reducevalues"
 import mergeLeft from "@unction/mergeleft"
+import key from "@unction/key"
 import fresh from "@unction/fresh"
 
 const FIRST = 0
 
 export default function mergeAllLeft (iterables: Array<IterableType<T>>): IterableType {
-  return reduceValues(mergeLeft)(fresh(iterables[FIRST]))(iterables)
+  return reduceValues(mergeLeft)(fresh(key(FIRST)(iterables)))(iterables)
 }
