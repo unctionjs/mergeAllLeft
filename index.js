1e302dc3b2dc2ad4ab8ba29361d4c81091d98702
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 19:46:28 -0700

    We have a new, more formal, name for iterables

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ import fresh from "@unction/fresh"
 
 const FIRST = 0
 
-export default function mergeAllLeft (iterables: Array<IterableType>): IterableType {
-  return reduceValues(mergeLeft)(fresh(key(FIRST)(iterables)))(iterables)
+export default function mergeAllLeft (functors: Array<FunctorType>): FunctorType {
+  return reduceValues(mergeLeft)(fresh(key(FIRST)(functors)))(functors)
 }
