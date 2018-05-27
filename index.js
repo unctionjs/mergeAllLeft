744d070c1ce6bacf9736c1b6920a97242a9957fe
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:29 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-import reduceValues from "@unction/reducevalues"
-import mergeLeft from "@unction/mergeleft"
-import first from "@unction/first"
-import fresh from "@unction/fresh"
-
-export default function mergeAllLeft (functors: Array<FunctorType>): FunctorType {
+import reduceValues from "@unction/reducevalues";
+import mergeLeft from "@unction/mergeleft";
+import first from "@unction/first";
+import fresh from "@unction/fresh";
+export default function mergeAllLeft(functors) {
   if (first(functors)) {
-    return reduceValues(mergeLeft)(fresh(first(functors)))(functors)
+    return reduceValues(mergeLeft)(fresh(first(functors)))(functors);
   }
 
-  return functors
+  return functors;
 }
