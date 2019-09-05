71d5b998b961246ff3ab5e4bb6b73baa75df1db8
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 01:17:25 -0700

    Annotating, fixing issues with packages

--- index.ts
+++ index.ts
@@ -2,10 +2,12 @@ import reduceValues from "@unction/reducevalues";
 import mergeLeft from "@unction/mergeleft";
 import first from "@unction/first";
 import fresh from "@unction/fresh";
-export default function mergeAllLeft (functors) {
-  if (first(functors)) {
-    return reduceValues(mergeLeft)(fresh(first(functors)))(functors);
+import {EnumerableType} from "./types";
+
+export default function mergeAllLeft<A> (enumerables: Array<EnumerableType<A>>): EnumerableType<A> {
+  if (first(enumerables)) {
+    return reduceValues(mergeLeft)(fresh(first(enumerables)))(enumerables);
   }
 
-  return functors;
+  return enumerables;
 }
