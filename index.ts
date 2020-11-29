10ddbb3a8ff794093ab75ef163159b600e814ee3
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-28 18:50:20 -0800

    Denormalizing some types to make things a little bit easier

--- index.ts
+++ index.ts
@@ -2,9 +2,8 @@ import reduceValues from "@unction/reducevalues";
 import mergeLeft from "@unction/mergeleft";
 import first from "@unction/first";
 import fresh from "@unction/fresh";
-import {EnumerableType} from "./types";
 
-export default function mergeAllLeft<A> (enumerables: Array<EnumerableType<A>>): EnumerableType<A> {
+export default function mergeAllLeft<A> (enumerables: Array<Array<A> | Set<A> | RecordType<unknown, A> | string>): Array<A> | Set<A> | RecordType<unknown, A> | string {
   if (first(enumerables)) {
     return reduceValues(mergeLeft)(fresh(first(enumerables)))(enumerables);
   }
