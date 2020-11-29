ed8b55733bbe47136a766fe744c9f3a2817e8dd9
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:50 -0800

    Removing more bad types

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import mergeLeft from "@unction/mergeleft";
 import first from "@unction/first";
 import fresh from "@unction/fresh";
 
-export default function mergeAllLeft<A> (enumerables: Array<Array<A> | Set<A> | RecordType<unknown, A> | string>): Array<A> | Set<A> | RecordType<unknown, A> | string {
+export default function mergeAllLeft<A> (enumerables: Array<Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string>): Array<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string {
   if (first(enumerables)) {
     return reduceValues(mergeLeft)(fresh(first(enumerables)))(enumerables);
   }
